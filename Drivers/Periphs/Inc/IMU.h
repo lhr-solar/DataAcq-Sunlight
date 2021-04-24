ff64f346aedb832215a75f1e31689fe7844e83d4
Manthan Upadhyaya <manthanand@gmail.com>
2021-04-24 11:43:57 -0500

    Created IMU Drivers

--- Drivers/Periphs/Inc/IMU.h
+++ Drivers/Periphs/Inc/IMU.h
@@ -7,13 +7,25 @@
 #define GYR_CONFIG 0x0A
 #define MAG_CONFIG 0x09
 #define UNIT_SEL 0x3B
-#define DEV_ADDR 0x28
+#define DEV_ADDR 0x2800 //Device address must be shifted to the left by 8 bits when using HAL_I2C library
 #define ACC_DATA_Z_MSB 0x0D
 #define ACC_DATA_Z_LSB 0x0C
 #define ACC_DATA_Y_MSB 0x0B
 #define ACC_DATA_Y_LSB 0x0A
 #define ACC_DATA_X_MSB 0x09
 #define ACC_DATA_X_LSB 0x08
+#define GYR_DATA_X_LSB 0x14
+#define GYR_DATA_X_MSB 0x15
+#define GYR_DATA_Y_LSB 0x16
+#define GYR_DATA_Y_MSB 0x17
+#define GYR_DATA_Z_LSB 0x18
+#define GYR_DATA_Z_MSB 0x19
+#define MAG_DATA_X_LSB 0x0E
+#define MAG_DATA_X_MSB 0x0F
+#define MAG_DATA_Y_LSB 0x10
+#define MAG_DATA_Y_MSB 0x11
+#define MAG_DATA_Z_LSB 0x12
+#define MAG_DATA_Z_MSB 0x13
 
 #include "main.h"
 #include <stdlib.h>
@@ -29,8 +41,8 @@ typedef struct {
     int16_t gyr_x;
     int16_t gyr_y;
     int16_t gyr_z;
-    int16_t accel_temp;
-    int16_t gyr_temp;
+    //int16_t accel_temp;
+    //int16_t gyr_temp;
 } IMUData_t;
 
 void IMU_Init(void);
--- Drivers/Periphs/Src/IMU.c
+++ Drivers/Periphs/Src/IMU.c
@@ -3,15 +3,27 @@
 IMUData_t IMUData;
 
 void IMU_Init(){
-    uint16_t config = OPR_MODE << 8;
-    config |= 7;
-    HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, config, 1); //Turn on Accelerometer, Gyroscope, and Magnetometer
-    config = 0;
-    config = UNIT_SEL << 8;
-    config &= 0xFF00;
-    HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, config, 1); //Read in m/s^2, Celcius, and degrees
+    uint8_t config[2];
+    config[0] = OPR_MODE; //register address
+    config[1] = 7; //data to be transmitted 
+    HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, config, 2); //Turn on Accelerometer, Gyroscope, and Magnetometer 
+    config[0] = UNIT_SEL;
+    config[1] = 0;
+    HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, config, 2); //Read in m/s^2, Celcius, and degrees
 }
 
 void IMU_Rx(){
-
+    uint8_t reg_addr = 8;
+    uint8_t data[18];
+    HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, &reg_addr, 1); //send register address to read from
+    HAL_I2C_Master_Receive_IT(&hi2c1, DEV_ADDR, data, 18);
+    IMUData.accel_x = (data[0] << 8) | data[1];
+    IMUData.accel_y = (data[2] << 8) | data[3];
+    IMUData.accel_z = (data[4] << 8) | data[5];
+    IMUData.mag_x = (data[6] << 8) | data[7];
+    IMUData.mag_y = (data[8] << 8) | data[9];
+    IMUData.mag_z = (data[10] << 8) | data[11];
+    IMUData.gyr_x = (data[12] << 8) | data[13];
+    IMUData.gyr_y = (data[14] << 8) | data[15];
+    IMUData.gyr_z = (data[16] << 8) | data[17];
 }
