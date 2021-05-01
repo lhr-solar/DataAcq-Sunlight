0309b11243499bcba84e822851a0b2fc40551d33
Manthan Upadhyaya <manthanand@gmail.com>
2021-05-01 14:23:06 -0500

    Made code more efficient

--- Drivers/Periphs/Src/IMU.c
+++ Drivers/Periphs/Src/IMU.c
@@ -15,16 +15,16 @@ void IMU_Init(IMUData_t* Data){
 
 void IMU_UpdateMeasurements(){
     uint8_t reg_addr = 8;
-    uint8_t data[18];
+    uint16_t data[9];
     HAL_I2C_Master_Transmit_IT(&hi2c1, DEV_ADDR, &reg_addr, 1); //send register address to read from
     HAL_I2C_Master_Receive_IT(&hi2c1, DEV_ADDR, data, 18);
-    (*IMUData).accel_x = (data[0] << 8) | data[1];
-    (*IMUData).accel_y = (data[2] << 8) | data[3];
-    (*IMUData).accel_z = (data[4] << 8) | data[5];
-    (*IMUData).mag_x = (data[6] << 8) | data[7];
-    (*IMUData).mag_y = (data[8] << 8) | data[9];
-    (*IMUData).mag_z = (data[10] << 8) | data[11];
-    (*IMUData).gyr_x = (data[12] << 8) | data[13];
-    (*IMUData).gyr_y = (data[14] << 8) | data[15];
-    (*IMUData).gyr_z = (data[16] << 8) | data[17];
+    (*IMUData).accel_x = data[0];
+    (*IMUData).accel_y = data[1];
+    (*IMUData).accel_z = data[2];
+    (*IMUData).mag_x = data[3];
+    (*IMUData).mag_y = data[4];
+    (*IMUData).mag_z = data[5];
+    (*IMUData).gyr_x = data[6];
+    (*IMUData).gyr_y = data[7];
+    (*IMUData).gyr_z = data[8];
 }
