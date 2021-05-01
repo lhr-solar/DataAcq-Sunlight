46369860404342f2f4617237812b3ad4c4e596e4
Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
2021-05-01 15:43:35 -0500

    Fixed more typos

--- Drivers/Periphs/Src/RTC.c
+++ Drivers/Periphs/Src/RTC.c
@@ -13,7 +13,7 @@ ErrorStatus RTC_Init(RTCData_t *data){
     uint8_t config[2];
     config[0] = CONTROL_1; //register address
     config[1] = 0x08; //data to be transmitted 
-    HAL_I2C_Master_Transmit_IT(&hi2c2, DEVICE_ADDRESS, config, 2); // select 12 hour mode 
+    error = HAL_I2C_Master_Transmit_IT(&hi2c2, DEVICE_ADDRESS, config, 2); // select 12 hour mode 
     if (error != HAL_OK){return ERROR;}
     RTCData = data;
     return SUCCESS;
@@ -25,9 +25,9 @@ ErrorStatus RTC_Init(RTCData_t *data){
 ErrorStatus RTC_UpdateMeasurements(){
     uint8_t data[7];
     uint8_t reg_addr = SECONDS;
-    HAL_I2C_Master_Transmit_IT(&hi2c2, DEVICE_ADDRESS, &reg_addr, 1); //send register address to read from
+    error = HAL_I2C_Master_Transmit_IT(&hi2c2, DEVICE_ADDRESS, &reg_addr, 1); //send register address to read from
     if (error != HAL_OK){return ERROR;}
-    HAL_I2C_Master_Receive_IT(&hi2c2, DEVICE_ADDRESS, data, 7);
+    error = HAL_I2C_Master_Receive_IT(&hi2c2, DEVICE_ADDRESS, data, 7);
     if (error != HAL_OK){return ERROR;}
     RTCData->sec = data[0];
     RTCData->min = data[1];
