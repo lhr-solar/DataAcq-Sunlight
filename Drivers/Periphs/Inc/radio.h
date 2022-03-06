3a9f1a8e0ed63a842059dd1a9443854e295f21a3
Manthan Upadhyaya <manthanand@gmail.com>
2022-03-06 16:58:15 -0600

    Added function to put data in queue, added comments for documentation, fixed function names

--- Drivers/Periphs/Inc/radio.h
+++ Drivers/Periphs/Inc/radio.h
@@ -10,6 +10,7 @@
 #include <stdint.h>
 #include <string.h>
 #include "CANBus.h"
+<<<<<<< HEAD
 #include "lwip.h"
 #include "IMU.h"
 #include "GPS.h"
@@ -27,6 +28,13 @@ int Ethernet_Init(int *lsocket);
 void sendMessage();
 void endConnection(int lsocket);
 >>>>>>> finished radio.c radio.h and broadcasting - need to test and debug
+=======
+#include "IMU.h"
+#include "GPS.h"
+
+#define ETHERNET_SIZE 256
+
+>>>>>>> Added function to put data in queue, added comments for documentation, fixed function names
 typedef enum{
     // this is where you have different types for the different messages that you might have 
     // for instance one for can 
@@ -35,10 +43,14 @@ typedef enum{
     GPS = 0x2,
     CAN = 0x3
 <<<<<<< HEAD
+<<<<<<< HEAD
 } EthernetID_t;
 =======
 } ethernetID;
 >>>>>>> changes to radio.c,radio.h, and braodcastingTask.c
+=======
+} EthernetID_t;
+>>>>>>> Added function to put data in queue, added comments for documentation, fixed function names
 typedef union {
     // one need to use on of these - depends on the length of the data 
 	CANMSG_t CANData;
@@ -58,18 +70,29 @@ typedef struct{
  * @brief Initialize Ethernet, create queue to hold messages and allocate
  *        socket when connection has been established
  * 
+<<<<<<< HEAD
+=======
+ * @param lsocket pointer to socket to allocated in ethernet
+>>>>>>> Added function to put data in queue, added comments for documentation, fixed function names
  * @return ErrorStatus ERROR if socket could not be binded to local address
  *                     ERROR if socket did not receive connection request
  *                     SUCCESS if socket was created successfully
  */
+<<<<<<< HEAD
 ErrorStatus Ethernet_Init(void);
 
 /** Ethernet Put in queue
+=======
+ErrorStatus Ethernet_Init(int *lsocket);
+
+/** Ethernet CollectMessage
+>>>>>>> Added function to put data in queue, added comments for documentation, fixed function names
  * @brief Put data in Ethernet Queue
  * 
  * @param msg Data to place in queue
  * @return BaseType_t - pdTrue if placed, pdFalse if full
  */
+<<<<<<< HEAD
 BaseType_t Ethernet_PutInQueue(EthernetMSG_t* msg);
 
 /** Ethernet Send Message
@@ -82,5 +105,21 @@ BaseType_t Ethernet_SendMessage(void);
  * @brief Close ethernet connection
  */
 void Ethernet_EndConnection(void);
+=======
+BaseType_t Ethernet_CollectMessage(EthernetMSG_t* msg);
+
+/** Ethernet Send Message
+ * @brief Send data from Ethernet Fifo across ethernet. Blocking: This will
+ *        wait until there is data in the queue to send it across
+ */
+void Ethernet_SendMessage(void);
+
+/** Ethernet End Connection
+ * @brief Close ethernet connection
+ * 
+ * @param lsocket socket to close connection
+ */
+void Ethernet_EndConnection(int lsocket);
+>>>>>>> Added function to put data in queue, added comments for documentation, fixed function names
 
 #endif
