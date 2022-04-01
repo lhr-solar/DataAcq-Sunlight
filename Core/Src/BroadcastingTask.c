1f5db8790442b943f58beae4ba34b3f9081c5c9b
Manthan Upadhyaya <manthanand@gmail.com>
2022-03-31 21:56:23 -0500

    Code cleanup

--- Core/Src/BroadcastingTask.c
+++ Core/Src/BroadcastingTask.c
@@ -2,7 +2,6 @@
 #include "radio.h"
 
 void BroadcastingTask(void *argument){
-    // this one is the while loop one - you put all the while loop things in here
     int lsocket;
     Ethernet_Init(&lsocket); //NOTE: need to handle returned error later
     while (1){
