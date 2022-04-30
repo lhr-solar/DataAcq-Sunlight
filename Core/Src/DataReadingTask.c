0dd2f417828e653d76ec68dc5f89555e373ffcbc
kevingonz19 <kevingonz@utexas.edu>
2022-04-30 15:48:26 -0500

    added finished & untested DataReadingTask

--- Core/Src/DataReadingTask.c
+++ Core/Src/DataReadingTask.c
@@ -1,16 +1,15 @@
 #include "Tasks.h"
 #include "IMU.h"
-#include "RTC.h"
 #include "GPS.h"
 #include "SD.h" // TODO: double check that this is the right header name
 #include "radio.h"
 
 // TODO: The ethernet send functions return BaseType_t which is just a long I think. Is it jsut returning the type that was placed on the stack?
-
+//This function reads from IMU and GPS data structures, copies their current values, and sends them both through ethernet and to be saved on SD card.
+//TODO: include CAN data
 void DataReadingTask(void* argument){
     while(1) {
         IMUData_t IMU_Data;
-        RTCData_t RTC_Data;
         GPSData_t GPS_Data;
         EthernetMSG_t Message;
 
@@ -23,9 +22,6 @@ void DataReadingTask(void* argument){
         // ethernetFIFO function
 
         //SD card functions TODO
-
-        error = RTC_UpdateMeasurements();  // NOTE: RTC functions not done
-        // ethernet FIFO function
 
         error = GPS_UpdateMeasurements() // this function seems to create a struct and send data all within its scope
         GPS_Data = GPS_ReadData(GPS_Data);  // TODO: Double check this is the proper return
