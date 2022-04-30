488e91d4edaa4c8501651d2e81170664cd915be4
kevingonz19 <kevingonz@utexas.edu>
2022-04-30 15:50:07 -0500

    Actually added finished & untested DataReadingTask

--- Core/Src/DataReadingTask.c
+++ Core/Src/DataReadingTask.c
@@ -1,7 +1,8 @@
 #include "Tasks.h"
 #include "IMU.h"
 #include "GPS.h"
-#include "SD.h" // TODO: double check that this is the right header name
+#include "CANBus.h"
+// TODO: add SD.h header
 #include "radio.h"
 
 // TODO: The ethernet send functions return BaseType_t which is just a long I think. Is it jsut returning the type that was placed on the stack?
@@ -9,27 +10,38 @@
 //TODO: include CAN data
 void DataReadingTask(void* argument){
     while(1) {
+        CANMSG_t CAN_Data;
         IMUData_t IMU_Data;
         GPSData_t GPS_Data;
+
         EthernetMSG_t Message;
+        BaseType_t status;
 
         HAL_StatusTypeDef error;
 
-        error = IMU_GetMeasurements(*IMU_Data);
-        Message.id= 0x1
-        Message.length = sizeof(IMU_Data)  // TODO: Ensure this is correct
-        Message.data.IMU_Data = IMU_Data // TODO: Make sure we're passing by value and not by reference
-        // ethernetFIFO function
+        // Send IMU data
+        error = IMU_GetMeasurements(&IMU_Data);
+        Message.id= IMU;
+        Message.length = sizeof(IMU_Data);  // TODO: Ensure this is correct
+        //Message.data.IMUData = IMU_Data; // TODO: Make sure we're passing by value and not by reference
+        memcpy(&Message.data.IMUData, &IMU_Data, sizeof(IMU_Data));
+        status = Ethernet_PutInQueue(&Message);
 
-        //SD card functions TODO
+        // Send CANBus data
+        status = CAN_FetchMessage(&CAN_Data);  // TODO: Double check this is the proper return
+        Message.id= CAN;
+        Message.length = sizeof(CAN_Data);
+        //Message.data.CANData = CAN_Data; // TODO: Make sure we're passing by value and not by reference
+        memcpy(&Message.data.CANData, &CAN_Data, sizeof(CAN_Data));
+        status = Ethernet_PutInQueue(&Message);
 
-        error = GPS_UpdateMeasurements() // this function seems to create a struct and send data all within its scope
-        GPS_Data = GPS_ReadData(GPS_Data);  // TODO: Double check this is the proper return
-
-
-
-
-
+        // Send GPS data
+        status = GPS_ReadData(&GPS_Data);  // TODO: Double check this is the proper return
+        Message.id= GPS;
+        Message.length = sizeof(GPS_Data);
+        //Message.data.GPSData = GPS_Data; // TODO: Make sure we're passing by value and not by reference
+        memcpy(&Message.data.GPSData, &GPS_Data, sizeof(GPS_Data));
+        status = Ethernet_PutInQueue(&Message);
 
     }
 
