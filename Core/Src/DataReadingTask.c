cbb6cbc4cc5752c17fb68bd31fe632b03069bae5
kevingonz19 <89663800+kevingonz19@users.noreply.github.com>
2022-04-30 20:57:08 -0500

    entire untested data reading task (#30)
    * entire untested data reading task
    
    * SD Card Drivers and Data Logging Task (#31)
    
    * First try at SDCard driver library.
    
    * Added SDCard.c and its includes to makefile. No rule to build it tho
    
    * Fixed typos in SD card module and the makefile. Successfully builds now
    
    * Added description comments for globals in Main.h. Made several SD card functions return ErrorStatus instead of spinning infinitely if an error occurs.
    
    * Fixed compiler warnings.
    
    * Removed incorrect comments for myprintf() in SDCard.h. Added documentation for SDCard Driver.
    
    * Added warning for closing file system to documentation.
    
    * Fixed header guard.
    
    * Removed myprintf
    
    * First try at SDCard driver library.
    
    * Added SDCard.c and its includes to makefile. No rule to build it tho
    
    * Fixed typos in SD card module and the makefile. Successfully builds now
    
    * Added description comments for globals in Main.h. Made several SD card functions return ErrorStatus instead of spinning infinitely if an error occurs.
    
    * Fixed compiler warnings.
    
    * Removed incorrect comments for myprintf() in SDCard.h. Added documentation for SDCard Driver.
    
    * Added warning for closing file system to documentation.
    
    * Fixed header guard.
    
    * Removed myprintf
    
    * Added debugging mode and changed function headers
    
    * Basic outline of function to read and split up data into gps, imu, and can when printing to sd card.
    
    * Created an SDCard object that will categorize types of messages in order to make sorting and printing them to SD card easier. Still need to figure out how to cast GPS/CAN/IMU message  to a char* in order to use already existing "SDCard_Write" function.
    
    * Function to print and format data on SD card. IMU data reading works, need to check if CAN and GPS data can be printed individually, then test if all 3 types being sent simultaneously can be sorted.
    
    * IMU and CAN print write data to SD card. IMU is completely accurate, CAN has a few issues, and GPS doesn't write at all.
    
    * sdcard switch to csv export format
    
    * added SD card queue
    
    * Queue functionality tested for SD Card
    
    * Added function for reopening file system and wrote Data Logging Task
    
    * Added documentation
    
    * Changed sizes of buffers
    
    * Added more documentation
    
    * Changed init location and added documentation
    
    * Added Tiandas suggestions
    
    * Fixed compile issues
    
    Co-authored-by: Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
    Co-authored-by: Tasnia Bari <tasniabari@utexas.edu>
    Co-authored-by: Tianda Huang <huangtianda@gmail.com>
    
    * entire untested data reading task
    
    * looped CANbus data reading
    
    * changed comments
    
    * Update Core/Src/DataReadingTask.c
    
    Co-authored-by: Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
    
    * Code cleanup. Need to make sure memcpy's are correct
    
    * Replaced unecessary memcpys
    
    Co-authored-by: manthanand <55002301+manthanand@users.noreply.github.com>
    Co-authored-by: Sugam Arora <sugamarora@DESKTOP-EAPDOTJ.localdomain>
    Co-authored-by: Tasnia Bari <tasniabari@utexas.edu>
    Co-authored-by: Tianda Huang <huangtianda@gmail.com>
    Co-authored-by: Manthan Upadhyaya <manthanand@gmail.com>
    Co-authored-by: Tianda Huang <37414124+tiandahuang@users.noreply.github.com>

--- Core/Src/DataReadingTask.c
+++ Core/Src/DataReadingTask.c
@@ -1,5 +1,58 @@
 #include "Tasks.h"
+#include "IMU.h"
+#include "GPS.h"
+#include "CANBus.h"
+#include "SDCard.h"
+#include "radio.h"
+
+//This function reads from IMU, CAN, and GPS data structures, copies their current values, and sends them both through ethernet 
+// (and to be saved on SD card when SD card code is finished).
 
 void DataReadingTask(void* argument){
-    
+    while(1) {
+        CANMSG_t CANData;
+        IMUData_t IMUData;
+        GPSData_t GPSData;
+        SDCard_t SDCardData;
+        EthernetMSG_t EthMessage;   
+
+        //Send GPS data and log in SD card
+        if (GPS_ReadData(&GPSData) != pdFALSE) {
+            memcpy(SDCardData.time, GPSData.time, sizeof(GPSData.time)); //assign timestamp
+            EthMessage.id= GPS;
+            EthMessage.length = sizeof(GPSData);
+            EthMessage.data.GPSData = GPSData;
+            Ethernet_PutInQueue(&EthMessage);// TODO: Add error handling on Ethernet_PutInQueue()
+
+            SDCardData.id = GPS_SDCard;
+            SDCardData.data.GPSData = GPSData;
+            SDCard_PutInQueue(&SDCardData); // TODO: Add error handling 
+        }
+        else {} // TODO: error handling for GPS_ReadData()
+
+        // Send IMU data and log in SD card
+        if (IMU_GetMeasurements(&IMUData) != HAL_ERROR){
+            EthMessage.id = IMU;
+            EthMessage.length = sizeof(IMUData); 
+            EthMessage.data.IMUData = IMUData;
+            Ethernet_PutInQueue(&EthMessage);// TODO: Add error handling on Ethernet_PutInQueue()
+
+            SDCardData.id = IMU_SDCard;
+            SDCardData.data.IMUData = IMUData;
+            SDCard_PutInQueue(&SDCardData); // TODO: add error handling    
+        } 
+        else {} // TODO: error handling for IMU_GetMeasurements()
+
+        // retrive and send as many CAN EthMessages as possible
+        while (CAN_FetchMessage(&CANData) != pdFALSE) { 
+            EthMessage.id= CAN;
+            EthMessage.length = sizeof(CANData);
+            EthMessage.data.CANData = CANData;
+            Ethernet_PutInQueue(&EthMessage); // TODO: Add error handling on Ethernet_PutInQueue()
+            
+            SDCardData.id = CAN_SDCard;
+            SDCardData.data.CANData = CANData;
+            SDCard_PutInQueue(&SDCardData); // TODO: add error handling
+        }
+    }
 }
