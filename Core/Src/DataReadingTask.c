96aff6345e2e2a1747a777fdd52fbd4ddef5a245
kevingonz19 <kevingonz@utexas.edu>
2022-04-30 19:37:59 -0500

    looped CANbus data reading

--- Core/Src/DataReadingTask.c
+++ Core/Src/DataReadingTask.c
@@ -16,7 +16,21 @@ void DataReadingTask(void* argument){
 
         EthernetMSG_t Message;
         BaseType_t status;
+        BaseType_t CANBus_status;
+        BaseType_t GPS_status;
         HAL_StatusTypeDef error;
+
+        // Send GPS data
+        GPS_status = GPS_ReadData(&GPS_Data);
+        if (GPS_status != pdFALSE) {
+            Message.id= GPS;
+            Message.length = sizeof(GPS_Data);
+            memcpy(&Message.data.GPSData, &GPS_Data, sizeof(GPS_Data));
+            status = Ethernet_PutInQueue(&Message);// TODO: Add error handling on Ethernet_PutInQueue()
+
+            // retrieve GPS time field and use as timestamp in SD card logging
+            
+        } else {} // TODO: error handling for GPS_ReadData()
 
         // Send IMU data
         error = IMU_GetMeasurements(&IMU_Data);
@@ -24,24 +38,17 @@ void DataReadingTask(void* argument){
             Message.id= IMU;
             Message.length = sizeof(IMU_Data); 
             memcpy(&Message.data.IMUData, &IMU_Data, sizeof(IMU_Data)); // copy to pass by value and not reference
-            status = Ethernet_PutInQueue(&Message);
-        }
+            status = Ethernet_PutInQueue(&Message);// TODO: Add error handling on Ethernet_PutInQueue()
+        } else {} // TODO: error handling for IMU_GetMeasurements()
 
         // Send CANBus data
-        status = CAN_FetchMessage(&CAN_Data);
-        if (status != pdFALSE) {
+        CANBus_status = CAN_FetchMessage(&CAN_Data);
+        while (CANBus_status != pdFALSE) { // retrive and send as many CAN messages as possible
             Message.id= CAN;
             Message.length = sizeof(CAN_Data);
             memcpy(&Message.data.CANData, &CAN_Data, sizeof(CAN_Data));
-            status = Ethernet_PutInQueue(&Message);
-        }
-               // Send GPS data
-        status = GPS_ReadData(&GPS_Data);
-        if (status != pdFALSE) {
-            Message.id= GPS;
-            Message.length = sizeof(GPS_Data);
-            memcpy(&Message.data.GPSData, &GPS_Data, sizeof(GPS_Data));
-            status = Ethernet_PutInQueue(&Message);
+            status = Ethernet_PutInQueue(&Message); // TODO: Add error handling on Ethernet_PutInQueue()
+            CANBus_status = CAN_FetchMessage(&CAN_Data);
         }
     }
 }
